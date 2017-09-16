eb8c0459ceca97caf53482977179b8a17503cd23
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-09-15 17:00:09 -0700

    add to repo

---
+++ exec pge air risk.sql
@@ -0,0 +1,27 @@
+
+
+
+exec hai.sp_hai_pge_air_risk
+
+		 47, -- @facility_id int  = 47,
+		 null, --@subfacility_codes varchar (500),
+		 null, --@location_groups varchar (2000),
+		'ppp-apams-01a', -- @locations varchar (2000) =
+		  null, --@sample_type varchar(200),
+		 'potrero-pams' , --@task_codes varchar (1000) =
+		  null, --@SDG varchar (2000),
+		 'jan 01 1900 12:00 AM', --@start_date datetime= 
+		 'dec 31 2050 11:59 PM', --@end_date datetime=
+		 'pge potrero air risk vocs', --@analyte_groups varchar(2000) =
+		  null, --@cas_rns varchar (2000),
+		  null, --@analytic_methods varchar (2000),
+		  null, --@matrix_codes varchar (500),
+		 'ug/m3', --@target_unit varchar(100) = 
+		  'RL', --@limit_type varchar (10) =
+		'pge-sl-potrero-aa-noncarc', -- @action_level_codes varchar (500) =
+		 '# Q', --@user_qual_def varchar (10) =
+		 'y', --@show_val_yn varchar(10) =
+		  null, --@coord_type varchar (20),
+		 'N', --@detects_only varchar (10) =   /*returns all samples/chemicals if any one sample had that chemcial detected*/
+		 'AVG', --@aggregate varchar (20) = 
+		1 -- @dispersion_factor float = 1

60751c5e0c254e5b960e1d717cf587a01c2c0f15
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-11-02 17:41:20 -0700

    pge data share status
    updates

--- create sp_hai_pge_data_share_status.sql
+++ create sp_hai_pge_data_share_status.sql
@@ -18,7 +18,8 @@ begin
 	select distinct 
 		task_code
 		,lab_sdg
-		,date_received
+		,case when edd_user = 'TestAmerica' then 'TA Lab' else 'H&A' end as Loaded_by
+		,Date_Received as [date_received/loaded]
 		,elapsed_days
 		,case when elapsed_days > = @date_check_threshold and permission_type_code not like '0' and permission_type_code not like '999' then 'Check' else '--' end as status
 		,permission_type_code
@@ -35,6 +36,7 @@ begin
 		,t.ebatch as dt_test_ebatch
 		,email_edd.ebatch
 		,email_edd.file_name
+		,edd_user
 		,convert(varchar,email_edd.received_date, 101) as Date_Received
 		,datediff(d,received_date,getdate())  elapsed_days
 
@@ -57,15 +59,29 @@ begin
 			on s.facility_id = tp.facility_id and s.task_code = tp.task_code
 			where s.facility_id = @facility_id and s.sample_type_code = 'n')s
 			 on t.facility_id = s.facility_id and t.sample_id = s.sample_id
-		inner join (SELECT 
-				ebatch
-				, file_name
-				, received_date
-				FROM [EQuIS].[dbo].[st_file_registration]
-				where facility_id = @facility_id and received_date >= DATEADD(DAY, -@date_window,getdate()))email_edd
+		left join (select    
+					eb.ebatch
+					,t.lab_sdg
+					,reverse(case when charindex('\',reverse(eb.edd_file)) > 0 then left(reverse(eb.edd_file), charindex('\',reverse(eb.edd_file))-1) else reverse(eb.edd_file) end) as file_name
+					, edd_date as received_date
+					,edd_user
+					from st_edd_batch eb
+					inner join (
+						select distinct 
+							 t.facility_id
+							,t.sample_id
+							,t.ebatch
+							,lab_sdg 
+						from dt_test t 
+						inner join dt_sample s on t.facility_id = s.facility_id and t.sample_id = s.sample_id
+						where t.facility_id = 47 and s.sample_type_code = 'n') t 
+						on eb.ebatch = t.ebatch
+					where edd_type = 'equis 4-file format'
+						and facility_id = @facility_id 
+						and edd_date >= DATEADD(DAY, -@date_window,getdate()))email_edd
 			on t.ebatch = email_edd.ebatch
 		Where t.facility_id = @facility_id
 		and received_date >=  DATEADD(DAY, -@date_window,getdate()))z
-		Order by  date_received, lab_sdg
+		Order by  date_received desc, lab_sdg
 
 end
